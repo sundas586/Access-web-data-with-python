aefcf57e5fc5d94b2c92ebe0a2a3ab30bc344c6c
sundas586 <33677647+sundas586@users.noreply.github.com>
2022-10-04 08:49:15 -0700

    Update Sockets.py

--- Sockets.py
+++ Sockets.py
@@ -1,14 +1,21 @@
+# To open a socket and retrieve the data. 
+
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode() 
 
+mysock.send(cmd)
 while True:
     data = mysock.recv(512)
-    if len(data) < 1:
+    if len(data) < 1:  # if the file ends, the length becomes < 1 
         break
-    print(data.decode(),end='')
+    print(data.decode(),end='') # converting to uni-code
 
 mysock.close()
+
+'''GET http://data.pr4e.org/romeo.txt HTTP/1.0 -- (HTTP RULES)
+\n -- (Followed by endline)
+\n -- (Followed by empty line)
+.encode() -- utf8 encoding''''
