43ca4b9defefc486d2ee34924c77e7f552ab27f0
sundas586 <33677647+sundas586@users.noreply.github.com>
2022-10-04 21:22:13 -0700

    Update Sockets_Using_HTTP_urllib.py

--- Sockets_Using_HTTP_urllib.py
+++ Sockets_Using_HTTP_urllib.py
@@ -2,8 +2,9 @@
 # 3 lines code to retrive web data and use it as a (by using HTTP urllib as a socket).
 
 import urllib.request
-request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/')
+request_url = urllib.request.urlopen('https://www.geeksforgeeks.org/') # create a handle
 print(request_url.read())
 
+# on line 5, we did not do any GET command or encoding, just a create handle and pass-in the string.
 # instead of open() we use openurl() as if its not a webpage but a file, and we treat it as a file.
 # it can be a html file or a text file, etc
