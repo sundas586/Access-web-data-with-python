4cf444d9039a2af81b2ecefdf7b696ef35212a62
sundas586 <33677647+sundas586@users.noreply.github.com>
2022-10-05 12:29:49 -0700

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,7 +1,13 @@
 # Using BeautifulSoup library, to retrieve and parse HTML and pull out anchor tags.
 
 import urllib.request, urllib.parse, urllib.error   # HTML library
-from bs4 import BeautifulSoup                       # library to parse HTML 
+from bs4 import BeautifulSoup                       # library to parse HTML
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')                             # Asking for desired URLR
 html = urllib.request.urlopen(url, context=ctx).read()  # creating Handle to pass the URL # socket
